60fe42b764c46906a613bd36d8022322c5acabde
Theo Vincent <theovincentjourdat@gmail.com>
2024-07-17 15:10:27 +0200

    fix bug launcher

--- launch_job/lunar_lander/launch_cluster_adadqn.sh
+++ launch_job/lunar_lander/launch_cluster_adadqn.sh
@@ -10,11 +10,11 @@ if [[ $GPU = true ]]
 then
     submission_train_adadqn="sbatch -J $EXPERIMENT_NAME\_adadqn --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=750M --time=00:15:00 --gres=gpu:1 -p amd,amd2,amd3 \
     --output=experiments/lunar_lander/logs/$EXPERIMENT_NAME/AdaDQN/train_adadqn_%a.out \
-    launch_job/lunar_lander/train_adadqn.sh -e $EXPERIMENT_NAME $BASE_ARGS $ADADQN_ARGS -g"
+    launch_job/lunar_lander/train_adadqn.sh -e $EXPERIMENT_NAME $BASE_ARGS $HP_SEARCH_ARGS $ADADQN_ARGS -g"
 else
     submission_train_adadqn="sbatch -J $EXPERIMENT_NAME\_adadqn --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=1500M --time=03:30:00 -p amd,amd2,amd3 \
     --output=experiments/lunar_lander/logs/$EXPERIMENT_NAME/AdaDQN/train_adadqn_%a.out \
-    launch_job/lunar_lander/train_adadqn.sh -e $EXPERIMENT_NAME $BASE_ARGS $ADADQN_ARGS"
+    launch_job/lunar_lander/train_adadqn.sh -e $EXPERIMENT_NAME $BASE_ARGS $HP_SEARCH_ARGS $ADADQN_ARGS"
 fi
 
 $submission_train_adadqn
