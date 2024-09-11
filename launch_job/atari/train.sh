4cb7f5b3c836350565e6be1dfe77c3951e96ce17
Theo <theovincentjourdat@gmail.com>
2024-09-11 15:03:27 +0200

    share memory GPU

--- launch_job/atari/train.sh
+++ launch_job/atari/train.sh
@@ -7,6 +7,16 @@ LAST_SEED=$((N_PARALLEL_SEEDS * SLURM_ARRAY_TASK_ID))
 
 source env_gpu/bin/activate
 
+if [[ $N_PARALLEL_SEEDS = 1 ]]
+then
+    export XLA_PYTHON_CLIENT_MEM_FRACTION=0.89
+elif [[ $N_PARALLEL_SEEDS = 2 ]]
+then
+    export XLA_PYTHON_CLIENT_MEM_FRACTION=0.42
+else
+    export XLA_PYTHON_CLIENT_MEM_FRACTION=0.275
+fi
+
 for (( seed=$FIRST_SEED; seed<=$LAST_SEED; seed++ ))
 do
     python3 experiments/$ENV_NAME/$ALGO_NAME.py --experiment_name $EXPERIMENT_NAME --seed $seed $ARGS &> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/train_$seed.out & 
