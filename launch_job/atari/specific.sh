f08cfabd84e8618412b4e0b8e8e81cc2062a42ea
Theo <theovincentjourdat@gmail.com>
2024-09-13 23:30:31 +0200

    adapt hp space

--- launch_job/atari/specific.sh
+++ launch_job/atari/specific.sh
@@ -1,40 +1,38 @@
 #!/bin/bash
 
 SHARED_ARGS="--replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon 1 --gamma 0.99 --horizon 27_000 \
-    --n_training_steps_per_epoch 250_000 --update_to_data 4 --n_initial_samples 20_000 --cnn_n_layers_range 1 5 \
-    --cnn_n_channels_range 16 80 --cnn_kernel_size_range 2 10 --cnn_stride_range 1 5 --mlp_n_layers_range 0 2 \
-    --mlp_n_neurons_range 25 1024 --learning_rate_range 6 3"
+    --n_training_steps_per_epoch 250_000 --update_to_data 4 --n_initial_samples 20_000 --cnn_n_layers_range 1 3 \
+    --cnn_n_channels_range 16 64 --cnn_kernel_size_range 2 8 --cnn_stride_range 2 5 --mlp_n_layers_range 0 2 \
+    --mlp_n_neurons_range 25 512 --learning_rate_range 6 3"
 
-# for TARGET_UPDATE_FREQUENCY in 1000 4000 8000
-# do
-#     for HP_UPDATE_FREQUENCY_FACTOR in 10 20 30
-#     do
-#         HP_UPDATE_FREQUENCY=$(( $HP_UPDATE_FREQUENCY_FACTOR * $TARGET_UPDATE_FREQUENCY ))
-#         for EXPLOITATION_TYPE in elitism truncation
-#         do
-#             for RESET_MODE in reset no_reset
-#             do
-#                 if [[ $RESET_MODE = "reset" ]]
-#                 then
-#                     RESET_FLAG="--reset_weights"
-#                 else
-#                     RESET_FLAG=""
-#                 fi
-#                 ADADQN_ARGS="--experiment_name tuf_$TARGET_UPDATE_FREQUENCY\_huf_$HP_UPDATE_FREQUENCY\_$EXPLOITATION_TYPE\_$RESET_MODE\_Pong \
-#                     $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 10 --n_networks 5 \
-#                     --exploitation_type $EXPLOITATION_TYPE --epsilon_end 0.01 --epsilon_duration 250_000 \
-#                     --hp_update_frequency $HP_UPDATE_FREQUENCY $RESET_FLAG"
+for TARGET_UPDATE_FREQUENCY in 4000 8000
+do
+    for HP_UPDATE_FREQUENCY in 40000 80000 
+    do
+        for EXPLOITATION_TYPE in elitism truncation
+        do
+            for RESET_MODE in reset no_reset
+            do
+                if [[ $RESET_MODE = "reset" ]]
+                then
+                    RESET_FLAG="--reset_weights"
+                else
+                    RESET_FLAG=""
+                fi
+                ADADQN_ARGS="--experiment_name tuf_${TARGET_UPDATE_FREQUENCY}_huf_${HP_UPDATE_FREQUENCY}_${EXPLOITATION_TYPE}_${RESET_MODE}_Pong \
+                    $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 10 --n_networks 5 \
+                    --exploitation_type $EXPLOITATION_TYPE --epsilon_end 0.01 --epsilon_duration 250_000 \
+                    --hp_update_frequency $HP_UPDATE_FREQUENCY $RESET_FLAG"
 
-#                 launch_job/atari/cluster_adadqn.sh --first_seed 1 --last_seed 2 --n_parallel_seeds 2 $ADADQN_ARGS
-#                 launch_job/atari/cluster_adadqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $ADADQN_ARGS
-#             done
-#         done
-#         sleep 10m
-#     done
-# done
+                launch_job/atari/cluster_adadqn.sh --first_seed 1 --last_seed 2 --n_parallel_seeds 2 $ADADQN_ARGS
+                launch_job/atari/cluster_adadqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $ADADQN_ARGS
+            done
+        done
+    done
+done
 
 
-# for TARGET_UPDATE_FREQUENCY in 1000 4000 8000
+# for TARGET_UPDATE_FREQUENCY in 4000 8000
 # do
 #     for MIN_STEPS_EVALUATION in 200 1000 5000
 #     do
@@ -48,7 +46,7 @@ SHARED_ARGS="--replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon
 #                 else
 #                     RESET_FLAG=""
 #                 fi
-#                 SEARLDQN_ARGS="--experiment_name tuf_$TARGET_UPDATE_FREQUENCY\_mse_$MIN_STEPS_EVALUATION\_tp_1_$EXPLOITATION_TYPE\_$RESET_MODE\_Pong \
+#                 SEARLDQN_ARGS="--experiment_name tuf_${TARGET_UPDATE_FREQUENCY}_mse_${MIN_STEPS_EVALUATION}_tp_1_${EXPLOITATION_TYPE}_${RESET_MODE}_Pong \
 #                     $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 10 --n_networks 5 \
 #                     --exploitation_type $EXPLOITATION_TYPE --min_steps_evaluation $MIN_STEPS_EVALUATION \
 #                     --training_proportion 1 $RESET_FLAG"
@@ -61,10 +59,10 @@ SHARED_ARGS="--replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon
 #     done
 # done
 
-RSDQN_ARGS="--experiment_name hpupe_10_Pong $SHARED_ARGS --target_update_freq 8000 --n_epochs 100 \
-    --epsilon_end 0.01 --epsilon_duration 250_000 --hp_update_per_epoch 10"
-launch_job/atari/local_rsdqn.sh --first_seed 1 --last_seed 2 $RSDQN_ARGS
-launch_job/atari/local_rsdqn.sh --first_seed 3 --last_seed 5 $RSDQN_ARGS
+# RSDQN_ARGS="--experiment_name hpupe_10_Pong $SHARED_ARGS --target_update_freq 8000 --n_epochs 100 \
+#     --epsilon_end 0.01 --epsilon_duration 250_000 --hp_update_per_epoch 10"
+# launch_job/atari/local_rsdqn.sh --first_seed 1 --last_seed 2 $RSDQN_ARGS
+# launch_job/atari/local_rsdqn.sh --first_seed 3 --last_seed 5 $RSDQN_ARGS
 # launch_job/atari/cluster_rsdqn.sh --first_seed 1 --last_seed 2 --n_parallel_seeds 2 $RSDQN_ARGS
 # launch_job/atari/cluster_rsdqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $RSDQN_ARGS
 
--- tests/test_hp_space_gpu_limit.py
+++ tests/test_hp_space_gpu_limit.py
@@ -11,12 +11,12 @@ class TestHPSpaceGPULimit(unittest.TestCase):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         hp_space = {
-            "cnn_n_layers_range": [1, 5],
-            "cnn_n_channels_range": [16, 80],
-            "cnn_kernel_size_range": [2, 10],
-            "cnn_stride_range": [1, 5],
+            "cnn_n_layers_range": [1, 3],
+            "cnn_n_channels_range": [16, 64],
+            "cnn_kernel_size_range": [2, 8],
+            "cnn_stride_range": [2, 5],
             "mlp_n_layers_range": [0, 2],
-            "mlp_n_neurons_range": [25, 1024],
+            "mlp_n_neurons_range": [25, 512],
             "activations": list(ACTIVATIONS.values()),
             "losses": list(LOSSES.values()),
             "optimizers": list(OPTIMIZERS.values()),
@@ -24,12 +24,12 @@ class TestHPSpaceGPULimit(unittest.TestCase):
             "reset_weights": False,
         }
         self.hp_detail = {
-            "cnn_n_layers": 5,
-            "cnn_n_channels": 80,
-            "cnn_kernel_size": 10,
-            "cnn_stride": 5,
+            "cnn_n_layers": 3,
+            "cnn_n_channels": 64,
+            "cnn_kernel_size": 8,
+            "cnn_stride": 2,
             "mlp_n_layers": 2,
-            "mlp_n_neurons": 1024,
+            "mlp_n_neurons": 512,
             "idx_activation": 0,
             "idx_loss": 0,
             "idx_optimizer": 0,
@@ -39,8 +39,8 @@ class TestHPSpaceGPULimit(unittest.TestCase):
         self.agent = AdaDQN(
             jax.random.PRNGKey(self.np_key),
             (84, 84, 4),
-            6,
-            n_networks=5,
+            4,
+            n_networks=15,
             hp_space=hp_space,
             exploitation_type="elitism",
             hp_update_frequency=100,
@@ -49,6 +49,7 @@ class TestHPSpaceGPULimit(unittest.TestCase):
             update_to_data=1,
             target_update_frequency=1,
         )
+
         self.batch = [
             jnp.zeros((32, 84, 84, 4)),
             jnp.ones(32, dtype=jnp.int32),
@@ -68,6 +69,7 @@ class TestHPSpaceGPULimit(unittest.TestCase):
                     self.agent.hp_generator.from_hp_detail(hp_key, self.hp_detail)
                 )
                 self.agent.hp_details[idx_hp] = self.hp_detail
+                print(sum(x.size for x in jax.tree_leaves(self.agent.params[0])))
 
             self.agent.target_params = self.agent.params[0].copy()
 
