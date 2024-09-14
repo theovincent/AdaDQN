f5dc58b8832d732953f7517a133c9e1d5c07665a
Theo <theovincentjourdat@gmail.com>
2024-09-14 17:08:47 +0200

    launcher specific

--- launch_job/atari/specific.sh
+++ launch_job/atari/specific.sh
@@ -5,13 +5,14 @@ SHARED_ARGS="--replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon
     --cnn_n_channels_range 16 64 --cnn_kernel_size_range 2 8 --cnn_stride_range 2 5 --mlp_n_layers_range 0 2 \
     --mlp_n_neurons_range 25 512 --learning_rate_range 6 3"
 
+# -------------- AdaDQN --------------
 for TARGET_UPDATE_FREQUENCY in 4000 8000
 do
     for HP_UPDATE_FREQUENCY in 40000 80000 
     do
-        for EXPLOITATION_TYPE in elitism truncation
+        for EXPLOITATION_TYPE in elitism # elitism truncation
         do
-            for RESET_MODE in reset no_reset
+            for RESET_MODE in no_reset # reset no_reset
             do
                 if [[ $RESET_MODE = "reset" ]]
                 then
@@ -19,8 +20,8 @@ do
                 else
                     RESET_FLAG=""
                 fi
-                ADADQN_ARGS="--experiment_name tuf_${TARGET_UPDATE_FREQUENCY}_huf_${HP_UPDATE_FREQUENCY}_${EXPLOITATION_TYPE}_${RESET_MODE}_Pong \
-                    $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 10 --n_networks 5 \
+                ADADQN_ARGS="--experiment_name long_tuf_${TARGET_UPDATE_FREQUENCY}_huf_${HP_UPDATE_FREQUENCY}_${EXPLOITATION_TYPE}_${RESET_MODE}_Pong \
+                    $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 40 --n_networks 5 \
                     --exploitation_type $EXPLOITATION_TYPE --epsilon_end 0.01 --epsilon_duration 250_000 \
                     --hp_update_frequency $HP_UPDATE_FREQUENCY $RESET_FLAG"
 
@@ -32,9 +33,10 @@ do
 done
 
 
+# -------------- SEARL --------------
 # for TARGET_UPDATE_FREQUENCY in 4000 8000
 # do
-#     for MIN_STEPS_EVALUATION in 200 1000 5000
+#     for MIN_STEPS_EVALUATION in 8000 16000
 #     do
 #         for EXPLOITATION_TYPE in elitism truncation
 #         do
@@ -46,19 +48,19 @@ done
 #                 else
 #                     RESET_FLAG=""
 #                 fi
-#                 SEARLDQN_ARGS="--experiment_name tuf_${TARGET_UPDATE_FREQUENCY}_mse_${MIN_STEPS_EVALUATION}_tp_1_${EXPLOITATION_TYPE}_${RESET_MODE}_Pong \
+#                 SEARLDQN_ARGS="--experiment_name tuf_${TARGET_UPDATE_FREQUENCY}_mse_${MIN_STEPS_EVALUATION}_tp_1_${EXPLOITATION_TYPE}_${RESET_MODE}_half_gradient_Pong \
 #                     $SHARED_ARGS --target_update_freq $TARGET_UPDATE_FREQUENCY --n_epochs 10 --n_networks 5 \
 #                     --exploitation_type $EXPLOITATION_TYPE --min_steps_evaluation $MIN_STEPS_EVALUATION \
-#                     --training_proportion 1 $RESET_FLAG"
+#                     --training_proportion 0.5 $RESET_FLAG"
 
 #                 launch_job/atari/cluster_searldqn.sh --first_seed 1 --last_seed 2 --n_parallel_seeds 2 $SEARLDQN_ARGS
 #                 launch_job/atari/cluster_searldqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $SEARLDQN_ARGS
 #             done
 #         done
-#         sleep 10m
 #     done
 # done
 
+# -------------- RSDQN --------------
 # RSDQN_ARGS="--experiment_name hpupe_10_Pong $SHARED_ARGS --target_update_freq 8000 --n_epochs 100 \
 #     --epsilon_end 0.01 --epsilon_duration 250_000 --hp_update_per_epoch 10"
 # launch_job/atari/local_rsdqn.sh --first_seed 1 --last_seed 2 $RSDQN_ARGS
@@ -66,6 +68,7 @@ done
 # launch_job/atari/cluster_rsdqn.sh --first_seed 1 --last_seed 2 --n_parallel_seeds 2 $RSDQN_ARGS
 # launch_job/atari/cluster_rsdqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $RSDQN_ARGS
 
+# -------------- DEHB --------------
 # DEHBDQN_ARGS="--experiment_name minnephp_5_maxnephp_15_Pong $SHARED_ARGS --target_update_freq 8000 --n_epochs 100 \
 #     --epsilon_end 0.01 --epsilon_duration 250_000 --min_n_epochs_per_hp 5 --max_n_epochs_per_hp 15"
 # launch_job/atari/local_dehbdqn.sh --first_seed 1 --last_seed 2 $DEHBDQN_ARGS
