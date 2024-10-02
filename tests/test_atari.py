bda0c225dd4b2e4dab77e36773e301fa1eee2ceb
Theo Vincent <theovincentjourdat@gmail.com>
2024-10-02 11:06:38 +0200

    higher stride range min tests

--- tests/test_atari.py
+++ tests/test_atari.py
@@ -11,7 +11,7 @@ class TestAtari(unittest.TestCase):
             "--seed 1 --disable_wandb --replay_buffer_capacity 100 --batch_size 3 --update_horizon 1 --gamma 0.99 "
             + "--horizon 10 --n_epochs 3 --n_training_steps_per_epoch 5 --update_to_data 3 "
             + "--target_update_frequency 3 --n_initial_samples 3 --cnn_n_layers_range 1 3 --cnn_n_channels_range 12 64 "
-            + "--cnn_kernel_size_range 2 8 --cnn_stride_range 1 8 --mlp_n_layers_range 1 3 --mlp_n_neurons_range 25 200 "
+            + "--cnn_kernel_size_range 2 8 --cnn_stride_range 2 8 --mlp_n_layers_range 1 3 --mlp_n_neurons_range 25 200 "
             + "--activations celu elu tanh --losses huber l1 --optimizers adagrad nadam --learning_rate_range 4 2"
         )
 
